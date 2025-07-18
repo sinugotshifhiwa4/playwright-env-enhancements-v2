2dea3c5c3c06f0e48ceb184c1a4a5bb92dadcc8a
Tshifhiwa Sinugo <tshifhiwa@amrod.co.za>
2025-07-18 23:42:44 +0200

    chore updated test to include only

--- tests/ui/OrangeHrm.spec.ts
+++ tests/ui/OrangeHrm.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from "@playwright/test";
 import EnvironmentVariables from "../../src/configuration/environment/variables/environmentVariables";
 import logger from "../../src/utils/logger/loggerManager";
 
-test("OrangeHrm", async ({ page }) => {
+test.only("OrangeHrm", async ({ page }) => {
   await page.goto(EnvironmentVariables.PORTAL_BASE_URL);
 
   expect(page).toHaveTitle("OrangeHRM");
