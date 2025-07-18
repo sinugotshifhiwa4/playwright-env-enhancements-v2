790a1ee06502e1e60ccfb2324d182cab3d66eb31
Tshifhiwa Sinugo <tshifhiwa@amrod.co.za>
2025-07-19 00:05:15 +0200

    unforatted code

--- tests/ui/OrangeHrm.spec.ts
+++ tests/ui/OrangeHrm.spec.ts
@@ -5,8 +5,8 @@ import logger from "../../src/utils/logger/loggerManager";
 test.only("OrangeHrm", async ({ page }) => {
   await page.goto(EnvironmentVariables.PORTAL_BASE_URL);
 
-  expect(page).toHaveTitle("OrangeHRM");
-  expect(page).toHaveURL(EnvironmentVariables.PORTAL_BASE_URL);
+                    expect(page).toHaveTitle("OrangeHRM");
+                   expect(page).toHaveURL(EnvironmentVariables.PORTAL_BASE_URL);
 
   logger.info(`Navigated to ${page.url()}`);
   await page.waitForTimeout(3000);
